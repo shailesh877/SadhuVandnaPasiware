388dafc4a623a605d794826fd843f7b1166458c2
shailesh877 <sadhuvandna23@gmail.com>
2026-02-03 19:46:53 +0530

    Specify port in MySQL connection
    Added port number to MySQL connection.

--- .vscode/Sadhuvandna-Api/connection.php
+++ .vscode/Sadhuvandna-Api/connection.php
@@ -1,6 +1,6 @@
 <?php
 // Re-using the same credentials as the legacy project
-$con=mysqli_connect("localhost","u941015828_sadhuvandna","Sadhuvandna7832%^","u941015828_sadhuvandna");
+$con=mysqli_connect("localhost","u941015828_sadhuvandna","Sadhuvandna7832%^","u941015828_sadhuvandna","3306");
 
 if (mysqli_connect_errno()) {
     echo json_encode(["status" => "error", "message" => "Failed to connect to MySQL: " . mysqli_connect_error()]);
