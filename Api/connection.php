cfd8d35ef3efde70e928c434d82953c57a829aae
Yuvraj Singh <sadhuvandna23@gmail.com>
2026-02-04 10:19:34 +0530

    update: production db connection config

--- Api/connection.php
+++ Api/connection.php
@@ -1,3 +1,3 @@
 <?php
-$con=mysqli_connect("mysql","u941015828_sadhuvandna","Sadhuvandna7832%^","u941015828_sadhuvandna"."3306");
+$con=mysqli_connect("mysql","u941015828_sadhuvandna","Sadhuvandna7832%^","u941015828_sadhuvandna",3306);
 ?>
