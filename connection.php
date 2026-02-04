5b75ae16d5746642a78946baee31308c417c4661
shailesh877 <sadhuvandna23@gmail.com>
2026-02-04 12:32:36 +0530

    Update database connection credentials

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-$con=mysqli_connect("mysql","u941015828_sadhuvandna","Sadhuvandna7832%^","u941015828_sadhuvandna",3306);
+$con=mysqli_connect("e4skgkwwk0s0gkso48oc40kg","u941015828_sadhuvandna","Sadhuvandna7832%^","u941015828_sadhuvandna",3306);
 ?>
