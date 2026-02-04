97400f6faed5fc0359b349bea1c1da5a17cd42e1
shailesh877 <sadhuvandna23@gmail.com>
2026-02-04 09:05:38 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,3 @@
 <?php
-$con=mysqli_connect("localhost","u941015828_sadhuvandna","Sadhuvandna7832%^","u941015828_sadhuvandna");
+$con=mysqli_connect("mysql","u941015828_sadhuvandna","Sadhuvandna7832%^","u941015828_sadhuvandna","3306");
 ?>
