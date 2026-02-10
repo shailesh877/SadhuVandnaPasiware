eef5e353a4853041e8ac4b2d5a3fdf3c8d190528
shailesh877 <sadhuvandna23@gmail.com>
2026-02-10 08:07:34 +0530

    Update database connection parameters

--- connection.php
+++ connection.php
@@ -1,9 +1,3 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-$con=mysqli_connect("localhost","u941015828_sadhuvandna","Sadhuvandna7832%^","u941015828_sadhuvandna");
-if (mysqli_connect_errno()) {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  exit();
-}
+$con=mysqli_connect("mysql","u941015828_sadhuvandna","Sadhuvandna7832%^","u941015828_sadhuvandna",3306);
 ?>
