c3c796db179ba96cb64b9b1b503389fd1c7405e6
shailesh877 <sadhuvandna23@gmail.com>
2026-02-14 15:12:38 +0530

    Fix variable name from '$conn' to '$con'

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 include "connection.php";
 
-$result = mysqli_query($conn, "SELECT news_description FROM tbl_news LIMIT 1");
+$result = mysqli_query($con, "SELECT news_description FROM tbl_news LIMIT 1");
 
 $row = mysqli_fetch_assoc($result);
 
