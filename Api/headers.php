76c9ec60d5d7766577887ce48d783a2b62e67037
shailesh877 <sadhuvandna23@gmail.com>
2026-03-12 16:37:04 +0530

    Update headers.php

--- Api/headers.php
+++ Api/headers.php
@@ -1,11 +1,5 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: *");
 header("Content-Type: application/json; charset=UTF-8");
-
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
-?>
+header("Access-Control-Allow-Methods: POST, GET");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
