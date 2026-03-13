01ac90e289fa181ac3266d9c3543c9425f3b79c5
shailesh877 <sadhuvandna23@gmail.com>
2026-03-13 15:44:37 +0530

    Update get_chat_user_status.php

--- Api/get_chat_user_status.php
+++ Api/get_chat_user_status.php
@@ -5,6 +5,8 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
 include("../php/connection.php");
+date_default_timezone_set("Asia/Kolkata");
+$con->query("SET time_zone = '+05:30'");
 
 // profile_id of the OTHER user (receiver)
 $receiver_id = $_GET['profile_id'] ?? 0;
@@ -17,13 +19,14 @@ if(!$receiver_id){
 }
 
 // 1. Get Receiver Status
+// Increased threshold to 300s (5 mins) for better stability
 if($platform === 'community'){
     // Community: receiver_id is member_id
-    $sql = "SELECT last_active, (UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(last_active) < 120) as is_online 
+    $sql = "SELECT last_active, (UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(last_active) < 300) as is_online 
             FROM tbl_members WHERE id='$receiver_id' LIMIT 1";
 } else {
     // Marriage: receiver_id is marriage_profile_id
-    $sql = "SELECT last_active, (UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(last_active) < 120) as is_online 
+    $sql = "SELECT last_active, (UNIX_TIMESTAMP(NOW()) - UNIX_TIMESTAMP(last_active) < 300) as is_online 
             FROM tbl_marriage_profiles WHERE id='$receiver_id' LIMIT 1";
 }
 
