d2572ecd4ce0a824a3cd2260e2609d9e22fbea06
shailesh877 <sadhuvandna23@gmail.com>
2026-03-13 15:37:15 +0530

    Update connection.php

--- Api/connection.php
+++ Api/connection.php
@@ -10,4 +10,5 @@ mysqli_set_charset($con, "utf8mb4");
 mysqli_query($con, "SET NAMES utf8mb4");
 mysqli_query($con, "SET CHARACTER SET utf8mb4");
 mysqli_query($con, "SET SESSION collation_connection = utf8mb4_unicode_ci");
+$con->query("SET time_zone = '+05:30'");
 ?>
