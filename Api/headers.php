c922c3dc1cd1b66dca355b6927bc4485f3400bf8
shailesh877 <sadhuvandna23@gmail.com>
2026-03-13 15:36:21 +0530

    Update headers.php

--- Api/headers.php
+++ Api/headers.php
@@ -3,3 +3,4 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, GET");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+date_default_timezone_set('Asia/Kolkata');
