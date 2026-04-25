fd7c528272f29614eb81fbc25aab8645199db5ce
shailesh877 <sadhuvandna23@gmail.com>
2026-04-25 17:58:51 +0530

    Add is_deleted column to tbl_messages
    Added 'is_deleted' column to 'tbl_messages' table.

--- Api/migration_add_is_deleted.php
+++ Api/migration_add_is_deleted.php
@@ -2,6 +2,9 @@
 include 'headers.php';
 include 'connection.php';
 
+// Disable strict mode for this session to avoid errors with existing 0000-00-00 dates
+$con->query("SET SESSION sql_mode = ''");
+
 // Add is_deleted to tbl_messages
 $con->query("ALTER TABLE `tbl_messages` ADD COLUMN `is_deleted` TINYINT(1) DEFAULT 0");
 
