fd4386387ecfd8be2c67dbdebf0417752fbbdb42
shailesh877 <sadhuvandna23@gmail.com>
2026-05-01 20:41:22 -0600

    Update include path for connection.php

--- Api/get_chat_user_status.php
+++ Api/get_chat_user_status.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
-include("../php/connection.php");
+include("connection.php");
 date_default_timezone_set("Asia/Kolkata");
 $con->query("SET time_zone = '+05:30'");
 
