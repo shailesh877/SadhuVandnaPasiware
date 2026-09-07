91ff9771aa35e2ebfbbd0eaaa23e277b12d109f2
Your Name <you@example.com>
2026-09-07 10:23:21 +0530

    Remove Api directory

--- Api/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysqli_connect("localhost", "u941015828_sadhuvandna", "Sadhuvandna7832%^", "u941015828_sadhuvandna");
-
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    exit();
-}
-?>
--- Api/headers.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST, GET");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-?>
